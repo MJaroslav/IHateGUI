d8f70a805d2b213aaaf32de3b8597b5d3a679f99
Yaroslav Novitsky <rcr3211@gmail.com>
2024-01-23 18:28:14 +0300

    add: jitci internal repository;

--- buildSrc/src/main/kotlin/maven-convention.gradle.kts
+++ buildSrc/src/main/kotlin/maven-convention.gradle.kts
@@ -9,5 +9,11 @@ afterEvaluate {
                 from(components["java"])
             }
         }
+        repositories {
+            maven {
+                name = "JitCI" // JitCI internal repository with fallback
+                url = uri(System.getenv("DEPLOY_DIR") ?: layout.buildDirectory.dir("deploy"))
+            }
+        }
     }
 }
